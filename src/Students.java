4c7b8a1772753b09b1d42138558b0ff9aebe5a43
urushak <khanalurusha73@gmail.com>
2025-01-14 12:34:22 +0545

    getter setter added

--- src/Students.java
+++ src/Students.java
@@ -8,4 +8,19 @@ public class Students {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
